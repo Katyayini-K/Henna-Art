76e3eda1d836353fa871677817e4b4e177ac6744
Katyayini-K <kkatyayini1@gmail.com>
2025-05-18 11:04:58 +0530

    booking appointment issue fixed

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,7 @@ const transporter = nodemailer.createTransport({
   secure: false, // Use TLS
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
